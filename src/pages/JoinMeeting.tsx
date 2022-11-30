006a40ad9cd7f1df5a9c964fa6fb352d52ca102a
Umidjon <rustamov.umidbek@gmail.com>
2022-11-30 13:35:46 +0500

    Join meeting completed

--- src/pages/JoinMeeting.tsx
+++ src/pages/JoinMeeting.tsx
@@ -1,7 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { firebaseAuth, meetingsRef } from "../utils/FirebaseConfig";
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate, useParams } from "react-router-dom";
+import useToast from "../hooks/useToast";
+import { query, where, getDocs } from "firebase/firestore";
+import moment from "moment";
 
 export default function JoinMeeting() {
-  return (
-    <div>JoinMeeting</div>
-  )
+  const [user, setUser] = useState<any>(undefined);
+  const params = useParams();
+  const navigate = useNavigate();
+  const [createToast] = useToast();
+  const [isAllowed, setIsAllowed] = useState(false);
+  const [userLoaded, setUserLoaded] = useState(false);
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) {
+      setUser(currentUser);
+    }
+    setUserLoaded(true);
+  });
+  useEffect(() => {
+    const getMeetingData = async () => {
+      if (params.id && userLoaded) {
+        const firestoreQuery = query(
+          meetingsRef,
+          where("meetingId", "==", params.id)
+        );
+        const fetchedMeetings = await getDocs(firestoreQuery);
+        if (fetchedMeetings.docs.length) {
+          const meeting = fetchedMeetings.docs[0].data();
+          const isCreator = meeting.createdBy === user?.uid;
+          if (meeting.meetingType === "1-on-1") {
+            if (meeting.invitedUsers[0] === user?.uid || isCreator) {
+              if (meeting.meetingDate === moment().format("L")) {
+                setIsAllowed(true);
+              } else if (
+                moment(meeting.meetingDate).isBefore(moment().format("L"))
+              ) {
+                createToast({ title: "Meeting has ended.", type: "danger" });
+                navigate(user ? "/" : "/login");
+              } else if (moment(meeting.meetingDate).isAfter()) {
+                createToast({
+                  title: `Meeting is on ${meeting.meetingDate}`,
+                  type: "warning",
+                });
+                navigate(user ? "/" : "/login");
+              }
+            } else {
+              navigate(user ? "/" : "/login");
+            }
+          } else if (meeting.meetingType === "video-conference") {
+            const index = meeting.invitedUsers.findIndex(
+              (invitedUser: string) => invitedUser === user?.uid
+            );
+            if (index !== -1 || isCreator) {
+              if (meeting.meetingDate === moment().format("L")) {
+                setIsAllowed(true);
+              } else if (
+                moment(meeting.meetingDate).isBefore(moment().format("L"))
+              ) {
+                createToast({ title: "Meeting has ended.", type: "danger" });
+                navigate(user ? "/" : "/login");
+              } else if (moment(meeting.meetingDate).isAfter()) {
+                createToast({
+                  title: `Meeting is on ${meeting.meetingDate}`,
+                  type: "warning",
+                });
+              }
+            } else {
+              createToast({
+                title: "You are not invited to the meeting",
+                type: "danger",
+              });
+              navigate(user ? "/" : "/login");
+            }
+          } else {
+            setIsAllowed(true);
+          }
+        } else {
+          navigate("/");
+        }
+      }
+    };
+    getMeetingData();
+  }, [userLoaded]);
+  return <div>JoinMeeting</div>;
 }
